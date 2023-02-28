b3aae7d71f7b1e3d698a36b6a2756fc76704b23f
fehsato <fyonezawa14@gmail.com>
2023-02-28 11:05:17 -0300

    js

---
+++ script.js
@@ -0,0 +1,43 @@
+function somar(){
+    var num1, num2, resp;
+    //ParseFloat(transformar string em número) O Floar é para números décimais, para números inteiros é int//
+    num1 = parseFloat(document.getElementById("num1").value);
+    num2 = parseFloat( document.getElementById("num2").value);
+    
+    resp = num1 + num2;
+
+    document.getElementById("resp").value = resp;
+}
+
+function subtrair(){
+    num1 = parseFloat(document.getElementById("num1").value);
+    num2 = parseFloat( document.getElementById("num2").value);
+
+    resp = num1 - num2;
+
+    document.getElementById("resp").value = resp;
+}
+
+function multiplicar(){
+    num1 = parseFloat(document.getElementById("num1").value);
+    num2 = parseFloat( document.getElementById("num2").value);
+
+    resp = num1 * num2;
+
+    document.getElementById("resp").value = resp;
+}
+
+function dividir(){
+    num1 = parseFloat(document.getElementById("num1").value);
+    num2 = parseFloat( document.getElementById("num2").value);
+
+    resp = num1 / num2;
+
+    document.getElementById("resp").value = resp;
+}
+
+function apagar(){
+    num1.delete()
+    
+   
+}
